050b84c7dca963a4b1950c18c0065695d228d0c7
Mahmoud Hamdy <mahmoudhamdy@Mahmouds-MacBook-Pro.local>
2023-05-03 01:44:26 +0300

    Generic Repository

--- Infastructure/Data/ProductRepository.cs
+++ Infastructure/Data/ProductRepository.cs
@@ -16,14 +16,27 @@ namespace Infastructure.Data
         {
             _context = context;
         }
+
+
+
         public async Task<Product> GetProductByIdAsync(int id)
         {
-           return await _context.Products.FindAsync(id);
+            return await _context.Products.Include(p => p.ProductBrand).Include(p => p.ProductType).FirstOrDefaultAsync(p => p.Id == id);
         }
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
-            return await _context.Products.ToListAsync();
+            return await _context.Products.Include(p => p.ProductBrand).Include(p => p.ProductType).ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductType>> GetProductTypeAsync()
+        {
+            return await _context.ProductTypes.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<ProductBrand>> GetProductBrandAsync()
+        {
+            return await _context.ProductBrands.ToListAsync();
         }
     }
 }
