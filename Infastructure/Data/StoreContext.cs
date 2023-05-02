698f6b16e55aaf71d2a2e23f3022fd52ca409a2a
mahmoudhamdy123 <m.hamdy@spiritude.co>
2023-05-02 12:46:21 +0300

    ReStructure

---
+++ Infastructure/Data/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace Infastructure .Data
+{
+	public class StoreContext:DbContext
+	{
+		public StoreContext(DbContextOptions options):base(options)
+		{
+
+		}
+
+		public DbSet<Product> Products { get; set; }
+		
+	}
+}
+
