5bf5b017a46bc5576080984f3c5a5e5ae52033e0
mahmoudhamdy123 <m.hamdy@spiritude.co>
2023-05-07 10:54:05 +0300

    Add APO

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿
+using Core.Entities;
+using Core.Interfaces;
+using Infastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}", Name = "GetProduct")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _productRepository.GetProductByIdAsync(id);
+            return Ok(product);
+        }
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,49 @@
+﻿using Core.Interfaces;
+using Infastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Infastructure.Data.StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefualtConnection"));
+});
+
+builder.Services.AddScoped<IProductRepository,ProductRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope  = app.Services.CreateScope();
+var Services  = scope.ServiceProvider;
+var context  = Services.GetRequiredService<StoreContext>();
+var Logger = Services.GetRequiredService<ILogger<Program>>();
+
+try{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}catch(Exception ex){
+    Logger.LogError(ex,"Error occurred while migrating process");
+}
+
+app.Run();
+
