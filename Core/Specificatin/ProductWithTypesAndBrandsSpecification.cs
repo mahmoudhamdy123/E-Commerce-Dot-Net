835811e37e59dc9298eba2e862cdfdfd82b3f212
mahmoudhamdy123 <m.hamdy@spiritude.co>
2023-05-14 17:34:48 +0300

    Add filtering

--- Core/Specificatin/ProductWithTypesAndBrandsSpecification.cs
+++ Core/Specificatin/ProductWithTypesAndBrandsSpecification.cs
@@ -9,10 +9,30 @@ namespace Core.Specifications
 {
     public class ProductWithTypesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductWithTypesAndBrandsSpecification()
+        public ProductWithTypesAndBrandsSpecification(string sort, int? brandId, int? typeId)
+        : base(x => (!brandId.HasValue || x.ProductBrandId == brandId) &&
+        (!typeId.HasValue || x.ProductTypeId == typeId))
         {
             AddIncludes(x => x.ProductType);
             AddIncludes(x => x.ProductBrand);
+            AddOrderBy(x => x.Name);
+
+
+            if (!string.IsNullOrEmpty(sort))
+            {
+                switch (sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(x => x.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(x => x.Price);
+                        break;
+                    default:
+                        AddOrderBy(x => x.Name);
+                        break;
+                }
+            }
         }
 
         public ProductWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
--- Infastructure/Data/StoreContext.cs
+++ Infastructure/Data/StoreContext.cs
@@ -3,23 +3,34 @@ using Microsoft.EntityFrameworkCore;
 using Core.Entities;
 using System.Reflection;
 
-namespace Infastructure .Data
+namespace Infastructure.Data
 {
-	public class StoreContext:DbContext
-	{
-		public StoreContext(DbContextOptions options):base(options)
-		{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
 
-		}
+        }
 
-		public DbSet<Product> Products { get; set; }
-		public DbSet<ProductBrand> ProductBrands { get; set; }
-		public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder); 
-			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+                {
+                    var properties = entityType.ClrType.GetProperties().Where(p => p.PropertyType == typeof(decimal));
+                    foreach (var property in properties)
+                    {
+                        modelBuilder.Entity(entityType.Name).Property(property.Name).HasConversion<double>();
+                    }
+                }
+            }
         }
 
     }
