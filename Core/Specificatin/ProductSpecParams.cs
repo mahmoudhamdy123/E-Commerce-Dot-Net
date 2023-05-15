60ffaff7f010a2a341d091f464058c03c0091e28
mahmoudhamdy123 <m.hamdy@spiritude.co>
2023-05-15 13:34:57 +0300

    Add search for product

--- Core/Specificatin/ProductSpecParams.cs
+++ Core/Specificatin/ProductSpecParams.cs
@@ -22,5 +22,12 @@ namespace Core.Specificatin
         public int? TypeId { get; set; }
 
         public string Sort { get; set; }
+
+        private string? _search;
+        public string? Search
+        {
+            get => _search;
+            set => _search = string.IsNullOrEmpty(value) ? null : value.ToLower();
+        }
     }
 }
--- Core/Specificatin/ProductWithFiltersForCountSepcifcation.cs
+++ Core/Specificatin/ProductWithFiltersForCountSepcifcation.cs
@@ -10,7 +10,8 @@ namespace Core.Specificatin
     public class ProductWithFiltersForCountSpecification : BaseSpecification<Product>
     {
         public ProductWithFiltersForCountSpecification(ProductSpecParams productParams) : base(
-            x => (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
+            x => (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+             (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
         (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
         )
         {
